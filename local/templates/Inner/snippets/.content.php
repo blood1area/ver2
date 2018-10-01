68e714b32de393f58e30fab458e3e10a16675a04
Дамир Буракаев <d.burakaev@newgrade.crt>
2018-10-01 17:54:23 +0300

    Добавлены загатовки для сниппетов

--- local/templates/Inner/snippets/.content.php
+++ local/templates/Inner/snippets/.content.php
@@ -1,5 +1,8 @@
 <?if(!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED!==true)die();?>
 <?
 $SNIPPETS = Array();
-$SNIPPETS['snippet0001.snp'] = Array('title' => 'Цитата справа');
+$SNIPPETS['snippet0001.snp'] = Array('title' => 'Цитата справа от текста');
+$SNIPPETS['snippet0002.snp'] = Array('title' => 'Цитата слева от текста');
+$SNIPPETS['snippet0003.snp'] = Array('title' => 'Обтекание картинки справа');
+$SNIPPETS['snippet0004.snp'] = Array('title' => 'Обтеание картинки слева');
 ?>
