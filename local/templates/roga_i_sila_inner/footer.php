30fc2ea4ce61517f1bf63a9481a5dd59946afd73
Дамир Буракаев <d.burakaev@newgrade.crt>
2018-10-08 12:29:57 +0300

    №97280 / Реализован шаблон для информационного меню в футере. Также залит файл style.css для шаблона search_form_header(где-то накосячил и он удалился)

--- local/templates/roga_i_sila_inner/footer.php
+++ local/templates/roga_i_sila_inner/footer.php
@@ -29,18 +29,22 @@ IncludeTemplateLangFile(__FILE__);
                         "SORT_TYPE" => "DESC"
                     )
                 );?>
-				<section class="info_block left_block_shadow">
-					<h2>Информация</h2>
-					<nav class="grey">
-						<ul>
-							<li><a href="#">О компании</a></li>
-							<li><a href="#">Контактная информация</a></li>
-							<li><a href="#">Условия продаж</a></li>
-							<li><a href="#">Финансовый отдел</a></li>
-							<li><a href="#">Для клиентов</a></li>
-						</ul>
-					</nav>
-				</section>
+                <?$APPLICATION->IncludeComponent(
+                    "bitrix:menu",
+                    "menu_footer",
+                    Array(
+                        "ALLOW_MULTI_SELECT" => "N",
+                        "CHILD_MENU_TYPE" => "",
+                        "DELAY" => "N",
+                        "MAX_LEVEL" => "1",
+                        "MENU_CACHE_GET_VARS" => array(""),
+                        "MENU_CACHE_TIME" => "3600",
+                        "MENU_CACHE_TYPE" => "N",
+                        "MENU_CACHE_USE_GROUPS" => "Y",
+                        "ROOT_MENU_TYPE" => "bottom",
+                        "USE_EXT" => "N"
+                    )
+                );?><br>
 			</section>
 			<div class="footer_inner">
 				<a href="http://www.qsoft.ru" target="_blank" class="qsoft grey inline-block">Сделано в</a>
