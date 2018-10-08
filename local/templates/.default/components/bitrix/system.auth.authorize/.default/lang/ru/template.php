dbdfa2908b63e8b1a8f9a31ad7598c8e71408fa5
Дамир Буракаев <d.burakaev@newgrade.crt>
2018-10-08 15:19:18 +0300

    №97281 / Кастомизирован шаблон компонента bitrix:system.auth.authorize Настроен вывод captcha в соответствии с ТЗ Добавлена ссылка на персональные данные при клике на имяФамилию компонента bitrix:system.auth.form в шапке(модернизирован шаблон auth_form_header, компонент не менялся) Добавлены требования к паролю (цифры+ниж и верх регистры) Удалены из шаблона отрывки кода, предназначенные для внешней авторизации Реализована переадресация при авторизации(backUrl)

---
+++ local/templates/.default/components/bitrix/system.auth.authorize/.default/lang/ru/template.php
@@ -0,0 +1,10 @@
+<?
+$MESS["TITLE"] = "Авторизация";
+$MESS["AUTH_PLEASE_AUTH"] = "Пожалуйста, авторизуйтесь:";
+$MESS["AUTH_LOGIN"] = "Логин:";
+$MESS["AUTH_PASSWORD"] = "Пароль:";
+$MESS["AUTH_REMEMBER_ME"] = "Запомнить меня";
+$MESS["AUTH_AUTHORIZE"] = "Войти";
+$MESS["AUTH_REGISTER"] = "Регистрация";
+$MESS["AUTH_FORGOT_PASSWORD_2"] = "Забыли свой пароль?";
+$MESS["AUTH_CAPTCHA_PROMT"] = "Введите слово на картинке";
--- local/templates/.default/components/bitrix/system.auth.form/auth_form_header/template.php
+++ local/templates/.default/components/bitrix/system.auth.form/auth_form_header/template.php
@@ -11,11 +11,10 @@ if($arResult["FORM_TYPE"] == "login"):?>
         <a href="<?=$arParams["REGISTER_URL"]?>?backurl=<?=$arResult["BACKURL"];?>" class="auth"><?=GetMessage("AUTH_LOGIN")?></a>
     </nav>
 <?
-else:
-?>
+else:?>
     <nav class="top_menu grey inline-block authorize">
         <span><?=GetMessage("ACCOST")?></span>
-        <a href="<?=$arResult["PROFILE_URL"]?>"><b class="user_name"><?=($arResult["USER_NAME"]?$arResult["USER_NAME"]:$arResult["USER_LOGIN"])?></b></a>
+        <a href="<?=$arResult["PROFILE_URL"]?>/profile/"><b class="user_name"><?=($arResult["USER_NAME"]?$arResult["USER_NAME"]:$arResult["USER_LOGIN"])?></b></a>
         <a href="<?=$arResult["PROFILE_URL"]?>"><?=GetMessage("AUTH_PROFILE")?></a>
         <a class="logout" href="
             <?=$APPLICATION->GetCurPageParam(
