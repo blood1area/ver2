afb273e339031bfa01a1d032c0280699099e0d35
Дамир Буракаев <d.burakaev@newgrade.crt>
2018-10-09 11:56:37 +0300

    №97282 / Отредактирован файл .section для правильного отображения в breadcrumb. Отредактирован файл /auth/index.php для вывода странички после регистрации Изменена стандартная форма регистрации в соответствии с тз(поля) Добавлен placeholder в поля email и password Создано почтовое событие USER_REGISTER, для него создан шаблон Обработчик события регистрации реализуется в init и ссылается на USER_REGISTER №97281 / Добавлен placeholder('*******') в поле password

--- local/php_interface/include/constants.php
+++ local/php_interface/include/constants.php
@@ -1,5 +1,7 @@
 <?php
+    define('PASSWORD_PLACEHOLDER', '*********');
+    define('EMAIL_PLACEHOLDER', 'name@site.domain');
+
     define('SITE_TEMPLATE_PATH_DEFAULT', '/local/templates/.default');
     define('NO_IMAGE_SRC', '/local/templates/.default/images/no-image.jpg');
     define('CACHE_TIME_DEFAULT', 3600);
-?>
--- local/php_interface/include/handlers.php
+++ local/php_interface/include/handlers.php
@@ -4,8 +4,7 @@ use \Bitrix\Main\EventManager;
 EventManager::getInstance()->addEventHandler(
     'main',
     'OnAfterUserAuthorize',
-    function ($arUser)
-    {
+    function ($arUser) {
         $arUserFields = $arUser["user_fields"];
         $mailContent = array(
             "EVENT_NAME" => "USER_AUTH",
@@ -15,6 +14,24 @@ EventManager::getInstance()->addEventHandler(
                 "LAST_LOGIN" => date("Y.d.m H:i:s"),
 //                "LAST_LOGIN" => \Bitrix\Main\Type\DateTime::createFromPhp(new \DateTime($arUserFields['LAST_LOGIN']))->format("Y.d.m H:i:s"),
                 "LOGIN" => $arUserFields["LOGIN"]
+            )
+        );
+        \Bitrix\Main\Mail\Event::send($mailContent);
+    }
+);
+
+EventManager::getInstance()->addEventHandler(
+    'main',
+    'OnAfterUserRegister',
+    function (&$arUser) {
+        $mailContent = array(
+            "EVENT_NAME" => "USER_REGISTER",
+            "LID" => "s1",
+            "C_FIELDS" => array(
+                'EMAIL' => $arUser['EMAIL'],
+                'LOGIN' => $arUser['LOGIN'],
+                'NAME' => $arUser['NAME'],
+                'LAST_NAME' => $arUser['LAST_NAME']
             )
         );
         \Bitrix\Main\Mail\Event::send($mailContent);
